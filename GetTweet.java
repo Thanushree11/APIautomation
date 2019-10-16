c2640d047e1258c73c42e7520ea50497e7d6456f
Thanushree11 <56435006+Thanushree11@users.noreply.github.com>
2019-10-16 17:23:00 +0530

    Add files via upload

---
+++ GetTweet.java
@@ -0,0 +1,41 @@
+   import org.testng.annotations.Test;
+
+	import io.restassured.RestAssured;
+	import io.restassured.response.Response;
+	
+
+	import static io.restassured.RestAssured.given;
+
+	import java.io.FileInputStream;
+	import java.util.Properties;
+
+	import io.restassured.path.json.JsonPath;
+
+	public class GetTweet {
+		
+		Properties prop= new Properties();
+		@Test
+		public void getTweet() throws Exception {
+			FileInputStream fls= new FileInputStream("C:\\Users\\Online Test\\eclipse-workspace\\APIAutomation\\data.properties");
+			prop.load(fls);
+			
+			String consumerKey = prop.getProperty("consumerKey");
+			String consumerSecret = prop.getProperty("consumerSecret");
+			String token = prop.getProperty("token");
+			String tokenSecret = prop.getProperty("tokenSecret");
+			
+			
+			RestAssured.baseURI= prop.getProperty("host");
+			Response res= given().auth().oauth(consumerKey, consumerSecret, token, tokenSecret).
+					when().get(Resource.getResource()).
+					then().extract().response();
+			String response= res.asString();
+			System.out.println(response);
+			JsonPath js= new JsonPath(response);
+			String text= js.get("text").toString();
+			System.out.println(text);
+		}
+
+	}
+
+
---
+++ Resource.java
@@ -0,0 +1,18 @@
+public class Resource {
+
+		public static String getResource() {
+			String res= "/home_timeline.json";
+			return res;
+		}
+
+		public static String postResource() {
+			String res= "/update.json";
+			return res;
+		}
+		
+		public static String retweetResource() {
+			String res= "/tweets.json";
+			return res;
+		}
+	}
+
